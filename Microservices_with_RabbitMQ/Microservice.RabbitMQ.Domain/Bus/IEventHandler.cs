487ea3dc635f364ae2ba071f20b7c2f9465e037a
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2020-02-05 00:59:22 +0600

    Implimented Interface for domain

---
+++ Microservices_with_RabbitMQ/Microservice.RabbitMQ.Domain/Bus/IEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microservice.RabbitMQ.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.RabbitMQ.Domain.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler 
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ Microservices_with_RabbitMQ/Microservice.RabbitMQ.Domain/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservice.RabbitMQ.Domain.Commands
+{
+    public abstract class Command
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
