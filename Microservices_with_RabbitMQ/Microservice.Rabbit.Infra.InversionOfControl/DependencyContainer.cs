fa2f6bd3ea3c13d4b96372d248393939b3707cf8
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2020-02-05 15:53:54 +0600

    Infra IoC added

---
+++ Microservices_with_RabbitMQ/Microservice.Rabbit.Infra.InversionOfControl/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microservice.RabbitMQ.Domain.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Microservice.Rabbit.Infra.InversionOfControl
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain
+            services.AddTransient<IEventBus, Microservices.RabbitMQ.Infra.Bus.RabbitMQ>();
+
+        }
+    }
+}
