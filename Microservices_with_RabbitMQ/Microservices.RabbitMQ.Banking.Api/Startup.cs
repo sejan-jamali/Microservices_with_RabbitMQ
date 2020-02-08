8bb2c03580d4675abd72711ef68c12637bd01447
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2020-02-09 01:23:54 +0600

    getting stuck

--- Microservices_with_RabbitMQ/Microservices.RabbitMQ.Banking.Api/Startup.cs
+++ Microservices_with_RabbitMQ/Microservices.RabbitMQ.Banking.Api/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microservice.Rabbit.Infra.InversionOfControl;
+using Microservices.RabbitMQ.Banking.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,16 @@ namespace Microservices.RabbitMQ.Banking.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            RegisterServices(services);
+            services.AddDbContext<BankingDBContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("BankingDBConnection"));
+            });
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
