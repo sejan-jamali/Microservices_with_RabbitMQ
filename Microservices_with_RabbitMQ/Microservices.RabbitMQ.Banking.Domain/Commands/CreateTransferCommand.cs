bf6eeb2bee64bea86ccb03b2e30f21b15f16110b
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2020-02-13 09:25:37 +0600

    Transferhandler bug

--- Microservices_with_RabbitMQ/Microservices.RabbitMQ.Banking.Domain/Commands/CreateTransferCommand.cs
+++ Microservices_with_RabbitMQ/Microservices.RabbitMQ.Banking.Domain/Commands/CreateTransferCommand.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MediatR;
 
 namespace Microservices.RabbitMQ.Banking.Domain.Commands
 {
-    public class CreateTransferCommand : TransferCommand
+    public class CreateTransferCommand : TransferCommand, IRequest<bool>
     {
         public CreateTransferCommand(int from, int to, decimal amount)
         {
