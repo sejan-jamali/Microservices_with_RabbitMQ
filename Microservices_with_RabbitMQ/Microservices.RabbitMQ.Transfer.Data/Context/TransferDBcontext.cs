3bb72ab4868896e33a3a33b18c1e3c09017ea9c9
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2020-02-14 23:53:07 +0600

    TDB init

---
+++ Microservices_with_RabbitMQ/Microservices.RabbitMQ.Transfer.Data/Context/TransferDBcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microservices.RabbitMQ.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.RabbitMQ.Transfer.Data.Context
+{
+    public class TransferDBcontext
+    {
+        public TransferDBcontext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
+    }
+}
---
+++ Microservices_with_RabbitMQ/Microservices.RabbitMQ.Transfer.Domain/Models/TransferLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.RabbitMQ.Transfer.Domain.Models
+{
+    public class TransferLog
+    {
+        public int Id { get; set; }
+        public string FromAccount { get; set; }
+        public decimal ToAccount { get; set; }
+        public decimal TransferAmount { get; set; }
+
+    }
+}
